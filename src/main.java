77326752774fa66d75e8ca9d85c5e1eef7b3e85f
Jamie Nolan <42931085+jamieNolanDEV@users.noreply.github.com>
2021-01-13 20:19:12 +0000

    Added Person Class

--- src/main.java
+++ src/main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
+        Person person = new Person("Jamie", 52);
+        HashTable hashTable = new HashTable();
+
+        hashTable.put("Jamie", "aaa");
+        hashTable.put("Jamie", "saf");
+        hashTable.put("Jamie", "sag");
+        hashTable.put("Jamie", "vxz");
+
+
 
     }
 }
